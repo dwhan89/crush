f5885f47b316eaf9279cab74fb8130c849d1e95c
Dongwon 'DW' Han <retimes@hotmail.com>
2019-10-24 14:42:08 -0400

    start automatic tile generation

--- bin/nemo_rusty_driver.py
+++ bin/nemo_rusty_driver.py
@@ -1,8 +1,9 @@
 from crush import config
 from soapack import interfaces as soint
+from pixell import enmap
 import os
 
-strict = True
+strict = False
 
 nemo_config_file = config.package_data_path('configs/nemo.yaml')
 nemo_config = config.read_yaml(nemo_config_file)
@@ -12,10 +13,11 @@ DM = soint.models['act_mr3']()
 
 patches = nemo_config['nemo']['patches']
 if type(patches) != list:
-    assert(patches in soint.models.keys()) # make sure data model is available
+    assert (patches in soint.models.keys())  # make sure data model is available
     # load all available patches in that model
     patches = DM.get_psa_indexes()
-else: pass
+else:
+    pass
 
 for patch in patches:
     season, patch, array, freq = patch.split('_')
@@ -30,10 +32,16 @@ for patch in patches:
         file_lists = [beam_file, map_file, weight_file]
         for file_path in file_lists:
             try:
-                assert(os.path.exists(file_path))
-            except:
+                assert (os.path.exists(file_path))
+            except AssertionError:
                 print("Missing File: %s" % file_path)
                 exit(1)
 
+    # build tile if needed
+    tile_setting = nemo_config['nemo']['tiles'].split('&')
+
+    # start automatic tile generation
+    if 'auto' in tile_setting:
+        ivar = enmap.read_fits(weight_file)
 print(patches)
 print(nemo_config)
