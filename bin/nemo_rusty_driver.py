1e2bd598f3cc87ed293c88647baecbf287f36712
DW <dwhan89@gmail.com>
2019-10-23 22:47:04 -0400

    intermediate updates

--- bin/nemo_rusty_driver.py
+++ bin/nemo_rusty_driver.py
@@ -1,1 +1,7 @@
+from crush import config
+import yaml
 
+nemo_config_file = config.package_data_path('configs/nemo.yaml')
+nemo_config = config.read_yaml(nemo_config_file)
+
+print(nemo_config)
---
+++ crush/config.py
@@ -0,0 +1,24 @@
+import os
+import warnings
+import yaml
+
+_root = os.path.abspath(os.path.dirname(__file__))
+
+
+def package_data_path(fname, strict=False):
+    file_path = os.path.join(_root, 'data', fname)
+    if not os.path.exists(file_path):
+        if strict:
+            raise FileNotFoundError("Can't find %s" % file_path)
+        else:
+            warnings.warn("Can't find %s" % file_path)
+    else:
+        pass
+    return file_path
+
+def read_yaml(yaml_file):
+    with open(yaml_file, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
